44194f3fbfcaec8a7758dcd13384f099fc90b07a
Strunk <strunk@MacBook-Pro.local>
2022-03-03 00:22:48 -0500

    first commit

---
+++ script.js
@@ -0,0 +1,39 @@
+// 1a
+let warmHugs = "Hi, I'm Olaf and I like warm hugs.";
+// 1b
+console.log(warmHugs.toUpperCase());
+// 1c
+console.log(warmHugs.replace("like", "love"));
+// 2a
+let beenImpaled = "Oh, look at that. I've been impaled.";
+// 2b
+console.log(beenImpaled.slice(-18));
+// 3a
+let dotDotDot = "...";
+// 3b
+let skullBones;
+skullBones = `I don't have a skull${dotDotDot}or bones.`;
+console.log(skullBones);
+// 4
+console.log(Math.PI);
+// 5 
+console.log(randomNumber = Math.floor(Math.random() * 3) + 1);
+// let randomNumber = Math.random();
+// randomNumber *= 3;
+// randomNumber++;
+// randomNumber = Math.floor(randomNumber);
+// randomNumber = Math.ceil(randomNumber);
+//console.log(randomNumber);
+
+// Bonus 6
+console.log(" Let It Go!".toUpperCase().repeat(3).slice(-21));
+// 7a
+let reindeers = "Reindeers are better than people.";
+// 7b
+console.log(reindeers.replace("Reindeers are better than people.", "Reindeers_are_better_than_people."));
+// 8
+let squareRoot = Math.sqrt(2);
+console.log(squareRoot);
+// 9
+console.log(newRandomNumber = Math.floor(Math.random() * 17) + 7);
+// to return a random integer between min and max: Math.floor(Math.random () * max - min + 1) + min >> floor sets min. * = max - min + 1
